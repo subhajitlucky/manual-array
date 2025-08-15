e9de21717ccf9c168b5b79fb6317bbc4d90f1f49
subhajitlucky <subhajitpradhan310@gmail.com>
2025-08-15 08:02:25 +0000

    pointer access comparison in array example

--- 02_access_with_pointers.cpp
+++ 02_access_with_pointers.cpp
@@ -3,3 +3,15 @@
 
 // Placeholder file — implement code described in README.md
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+
+    //access value via pointers
+
+    for(int i = 0;i<5;i++){
+        cout<<"Value : "<<arr[i]<<" "<<"Value via pointer : "<<*(arr+i)<<endl;
+    }
+}
