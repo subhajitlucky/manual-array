0da6666d625fff481e1554e362b54dbe3bd596d5
subhajitlucky <subhajitpradhan310@gmail.com>
2025-08-15 16:35:01 +0000

    push_back

--- 05_manual_push.cpp
+++ 05_manual_push.cpp
@@ -2,3 +2,41 @@
 // TODO: implement push_back(int) with capacity checks.
 
 // Placeholder file — implement code described in README.md
+// 04_safe_indexing.cpp
+// TODO: implement at(int index) with bounds checks and error handling.
+
+// Placeholder file — implement code described in README.md
+
+#include <iostream>
+using namespace std;
+
+struct FixedArray {
+    int arr[10];
+    size_t size;
+    size_t capacity;
+    int at(size_t index){
+        if(index>=0 && index<size){
+            return arr[index];
+        }
+        return -1;
+    }
+
+    void push_back(int value) {
+        if(size<capacity) {
+            arr[size] = value;
+            size++;
+        }else{
+            cout<<"No space"<<endl;
+        }
+    }
+};
+
+int main(){
+    FixedArray myArray = {{0}, 0, 1};
+    myArray.push_back(10);
+    myArray.push_back(20);
+
+
+    cout << "Element at index 0: " << myArray.at(0) << endl;
+    cout << "Element at index 1: " << myArray.at(1) << endl;
+}
