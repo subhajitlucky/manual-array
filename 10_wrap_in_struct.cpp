ee31742e6557e7ff833a155de02cb4059a090ade
subhajitlucky <subhajitpradhan310@gmail.com>
2025-08-19 15:39:18 +0000

    wrap_in_struct

--- 10_wrap_in_struct.cpp
+++ 10_wrap_in_struct.cpp
@@ -2,3 +2,59 @@
 // TODO: implement DynamicArray struct with ctor and dtor managing heap memory.
 
 // Placeholder file — implement code described in README.md
+
+
+#include <iostream>
+using namespace std;
+
+struct DynamicArray{
+    int* data;
+    int size;
+    int capacity;
+
+    DynamicArray(int capacity){
+        this->size = 0;
+        this->capacity = capacity;
+        this->data = new int[capacity];
+    }
+    ~DynamicArray(){
+        delete[] data;
+    }
+
+    void resize(){
+        int newCap = (capacity == 0 ? 1 : capacity * 2);
+        int* newArr = new int[newCap];
+
+        for(int i =0;i<size;i++){
+            newArr[i] = data[i];
+        }
+        delete[] data;
+        data = newArr;
+        capacity=newCap;
+    }
+
+    void push_back(int value){
+        if (size==capacity){
+            resize();
+        }
+        if(size<capacity){
+            data[size++] = value;
+        }
+    }
+};
+int main(){
+    DynamicArray array(5);
+    array.push_back(1);
+    array.push_back(2);
+    array.push_back(3);
+    array.push_back(4);
+    array.push_back(5);
+    array.push_back(6);
+
+    for (int i = 0; i < array.capacity; i++) {
+        cout << array.data[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
