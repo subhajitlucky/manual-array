fb632539bf89a0264ccd9b3904c12a64b109a6a4
subhajitlucky <subhajitpradhan310@gmail.com>
2025-08-23 16:15:37 +0000

    move

--- 12_move_semantics.cpp
+++ 12_move_semantics.cpp
@@ -2,3 +2,41 @@
 // TODO: implement move constructor and move assignment; demonstrate moves.
 
 // Placeholder file — implement code described in README.md
+
+#include <iostream>
+using namespace std;
+
+struct DynamicArray{
+    int* data;
+    size_t size;
+    size_t capacity;
+
+    //Constructor
+    DynamicArray(size_t cap){
+        this->capacity = cap;
+        this->size = 0;
+        this->data = new int[capacity];
+    }
+
+    //Destructor
+    ~DynamicArray(){
+        delete[] data;
+    }
+
+    //move
+    DynamicArray(DynamicArray&& other) {
+        this->data = other.data;
+        this->size = other.size;
+        this->capacity = other.capacity;
+
+        other.data = nullptr;
+        other.size = 0;
+        other.capacity = 0;
+    }
+};
+
+int main(){
+    DynamicArray arr1(10);
+    DynamicArray arr2 = std::move(arr1);
+
+}
